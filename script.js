c42a1650c454774c0bc0133e486b36a9382011f4
hodarasaei83 <hodarasaei83@gmail.com>
2025-12-09 14:40:09 +0330

    Add/example for scope-hoisting-closure

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('hello')
+var globalVar = "I'm global";
+
+function outerFunction() {
+    console.log("Before declaration:", hoistedVar);
+    
+    var hoistedVar = "I'm hoisted with var";
+    let letVar = "I'm declared with let";
+    const constVar = "I'm declared with const";
+    
+    console.log("After declaration:", hoistedVar);
+
+    function innerFunction() {
+        
+        console.log("From outer scope via closure:", letVar);
+        console.log("From global scope:", outerFunction.globalVar); 
+        
+        const capturedValue = letVar;
+        
+        return function() {
+            console.log("Nested closure accessing:", capturedValue);
+            console.log("Also can access:", constVar);
+        };
+    }
+    
+    return innerFunction;
+}
+
+console.log("Global variable:", globalVar);
+
+var resultFunc = outerFunction();
+
+var closure1 = resultFunc();
+closure1(); 
